5fc9663f2091f8ebce529846251e8f33de07c7de
Andrea Pollini <prof.andrea.pollini@gmail.com>
2024-01-11 18:26:18 +0100

    added texture, font and sound manager

--- include/SimpleGameKit/Core/AssetManager.h
+++ include/SimpleGameKit/Core/AssetManager.h
@@ -1,12 +1,68 @@
 #pragma once
+#include <Font.hpp>
+
+#include <Sound.hpp>
+#include <Texture.hpp>
+
+
 #include <map>
 
 #include "Utils.h"
 
 namespace sgk {
+template <typename T>
 class AssetManager {
-std::map<std::string,Texture2D> m_textures;
-public:
+  std::map<std::string, T> m_assets;
 
+public:
+  virtual ~AssetManager() = default;
+  AssetManager() = default;
+
+  void Load(const std::string& name, const std::string& path) {
+    m_assets[name] = LoadAsset(path);
+  }
+
+  T& Get(const std::string& name) {
+    return m_assets[name];
+  }
+
+  const T& Get(const std::string& name) const {
+    return m_assets[name];
+  }
+
+  void Unload(const std::string& name) {
+    m_assets.erase(name);
+  }
+
+  void UnloadAll() {
+    m_assets.clear();
+  }
+
+  size_t GetAssetCount() const {
+    return m_assets.size();
+  }
+
+  virtual T LoadAsset(const std::string& path) = 0;
 };
-}  // namespace sgk
+
+class TextureManager final : public AssetManager<raylib::Texture2D> {
+public:
+  raylib::Texture2D LoadAsset(const std::string& path) override {
+    return {path};
+  }
+};
+
+class FontManager final : public AssetManager<raylib::Font> {
+public:
+  raylib::Font LoadAsset(const std::string& path) override {
+    return {path};
+  }
+};
+
+class SoundManager final : public AssetManager<raylib::Sound> {
+public:
+  raylib::Sound LoadAsset(const std::string& path) override {
+    return {path};
+  }
+};
+} // namespace sgk
